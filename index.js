3ad0b81702214c0e39ec07b3a16eb8b2b51546aa
Keifer Shearer <keifearer@gmail.com>
2023-02-10 16:12:38 +0100

    Completed lab

--- index.js
+++ index.js
@@ -15,9 +15,31 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    const carrotCake = await Recipe.create({
+      title: 'Carrot Cakee',
+      level: 'Easy Peasy',
+      ingredients: 'carrot',
+      cuisine: 'cake',
+      dishType: 'dessert',
+      image: '',
+      duration: 30,
+      creator: 'Haroun',
+      created: '',
+    })
+    const manyRecipe = await Recipe.insertMany(data)
+    const filter = { title: 'Rigatoni alla Genovese' }
+    const update = { duration: 100 }
+    let updateRig = await Recipe.findOneAndUpdate(filter, update)
+    console.log('update succesful')
+    const deleteCake = await Recipe.deleteOne({ title: 'Carrot Cake' })
+    console.log('delete succesful')
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => {
+    mongoose.connection.close()
+  })
